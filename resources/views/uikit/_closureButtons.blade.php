c2993a7f8441e39d066eae07afe2c5953e441541
Davide Bronzini <davide_bronzini@hotmail.com>
2022-10-27 16:31:41 +0200

    submit buttons

--- resources/views/uikit/_closureButtons.blade.php
+++ resources/views/uikit/_closureButtons.blade.php
@@ -6,7 +6,17 @@
 	>
 
 @foreach($form->getClosureButtons() as $button)
-{!! $button->render() !!}
+	@if($button->isSubmit())
+
+	<input 
+		class="uk-button uk-button-primary"
+		type="submit"
+		name="{{ $button->getName() }}"
+		value="{{ $button->getText() }}"
+		>
+	@else
+		{!! $button->render() !!}
+	@endif
 @endforeach
 
 @if($cancel = $form->getCancelButton())
