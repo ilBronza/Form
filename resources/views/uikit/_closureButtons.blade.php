daf1aabd071baeec788c384b6cefdfb10c3ec159
Davide Bronzini <davide_bronzini@hotmail.com>
2023-03-17 09:11:55 +0100

    all

--- resources/views/uikit/_closureButtons.blade.php
+++ resources/views/uikit/_closureButtons.blade.php
@@ -8,12 +8,16 @@
 @foreach($form->getClosureButtons() as $button)
 	@if($button->isSubmit())
 
+		{!! $button->renderSubmit() !!}
+
+{{--
 	<input 
 		class="uk-button uk-button-primary"
 		type="submit"
 		name="{{ $button->getName() }}"
 		value="{{ $button->getText() }}"
 		>
+ --}}
 	@else
 		{!! $button->render() !!}
 	@endif
